53cb7de3bb8064fefff787fdb4241d2822c7d5e9
Sam Rubenacker <srubenacker@ucla.edu>
2016-11-12 18:54:26 -0800

    Added buttons to new connect view.

--- app/src/main/java/cs117/musync/BTConnect.java
+++ app/src/main/java/cs117/musync/BTConnect.java
@@ -3,16 +3,23 @@ package cs117.musync;
 import android.bluetooth.BluetoothDevice;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.UUID;
 
+// Bluetooth Library Link
+// https://android-arsenal.com/details/1/3071#!description
 import co.lujun.lmbluetoothsdk.BluetoothController;
 import co.lujun.lmbluetoothsdk.base.BluetoothListener;
+
 
 public class BTConnect extends AppCompatActivity {
 
     // For classic bluetooth
     BluetoothController mBTController = BluetoothController.getInstance().build(this);
+
+    private boolean server;
+    private boolean client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,5 +62,59 @@ public class BTConnect extends AppCompatActivity {
                 // Callback when remote device send data to current device.
             }
         });
+
+        server = false;
+        client = false;
+
+        if (!initializeBluetooth()) {
+            TextView statusTextView = (TextView) findViewById(R.id.statusTextView);
+            statusTextView.setText("Unable to initialize Bluetooth! :(");
+        }
     }
+
+
+    public boolean initializeBluetooth() {
+
+        if (!mBTController.isAvailable()) {
+            System.out.println("Bluetooth is not available! :(");
+            return false;
+        }
+
+        if (!mBTController.isEnabled()) {
+            System.out.println("Bluetooth is not enabled! :(");
+            return false;
+        }
+
+        if (!mBTController.openBluetooth()) {
+            
+        }
+
+
+        return true;
+    }
+
+
+    public void startAsServer() {
+        if (client == false) {
+            server = true;
+            client = false;
+            mBTController.startAsServer();
+            System.out.println("Starting as server...");
+        }
+    }
+
+
+    public void setAsDiscoverable() {
+        if (server == false) {
+            server = false;
+            client = true;
+            if (mBTController.setDiscoverable(60)) {
+                System.out.println("Setting as discoverable for 60 seconds...");
+            }
+            else {
+                client = false;
+            }
+        }
+    }
+
 }
